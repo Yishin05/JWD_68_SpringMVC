c0e335bb70f46a99824ca7f1eaae8641c1f1a4db
saiaungzawoo <saimon39999@gmail.com>
2026-05-20 12:03:18 +0630

    Update IndexController.java

--- src/main/java/com/spring/controller/IndexController.java
+++ src/main/java/com/spring/controller/IndexController.java
@@ -1,11 +1,15 @@
 package com.spring.controller;
 
 import org.springframework.stereotype.Controller;
+
+
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+
+//this is sai sai
 @Controller
 public class IndexController {
 
